bfb829066c4f05434c1bb92bd149a9ed61ab1bcb
Victoria Rybalko <rybalko.victoria@gmail.com>
2020-06-21 13:59:49 +0300

    Add checkMultiplicity function which checks whether the divisor is a multiple of the dividend.
    Added task description, JSDoc, function code, console logs of the test cases results.

--- index.js
+++ index.js
@@ -16,3 +16,27 @@ function isAdult(age) {
 
 console.log('isAdult(20) = ' + isAdult(20));
 console.log('isAdult(4) = ' + isAdult(4));
+
+/*2. Создать функцию checkMultiplicity, которая принимает два числа и проверяет кратность первого вторым:
+
+    checkMultiplicity(25, 5) // true
+    checkMultiplicity(15, 3) // true
+    checkMultiplicity(15, 5) // true
+    checkMultiplicity(15, 4) // false
+    
+ */
+
+/**
+ * Checks whether the divisor is a multiple of the dividend.
+ * @param dividend
+ * @param divisor
+ * @returns {boolean}
+ */
+function checkMultiplicity(dividend, divisor) {
+    return dividend % divisor === 0;
+}
+
+console.log('checkMultiplicity(25, 5) = ' + checkMultiplicity(25, 5));
+console.log('checkMultiplicity(15, 3) = ' + checkMultiplicity(15, 3));
+console.log('checkMultiplicity(15, 5) = ' + checkMultiplicity(15, 5));
+console.log('checkMultiplicity(15, 4) = ' + checkMultiplicity(15, 4));
